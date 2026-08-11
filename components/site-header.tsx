8e033c33c49b79404497f767b295a3b7d41a038a
swap <swaroop.hebbar7@gmail.com>
2026-08-11 00:59:43 -0400

    use lowercase nullspace branding (#7)

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -8,18 +8,18 @@ export function SiteHeader() {
         <a
           href="#top"
           className="inline-flex items-center gap-3 transition-opacity hover:opacity-60"
-          aria-label="NullSpace home"
+          aria-label="nullspace home"
         >
           <Image
             src="/nullspace-mark.png"
-            alt="NullSpace mark"
+            alt="nullspace mark"
             width={1024}
             height={1024}
             className="h-[50px] w-[50px] invert"
             priority
           />
           <span className="text-xl font-medium tracking-tight text-white">
-            NullSpace
+            nullspace
           </span>
         </a>
 
